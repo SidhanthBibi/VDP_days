28963774b57c2e6b3b61918ed9533fee4a0e53f7
Sidhanth Bibi <sidhanthbibi@gmail.com>
2024-11-04 16:53:59 +0530

    added a dummy property to the input field

--- vdp/components/ui/input.tsx
+++ vdp/components/ui/input.tsx
@@ -3,7 +3,10 @@ import * as React from "react"
 //import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+
+    _?: never; // Dummy property to satisfy ESLint
+  }
 
   const Input: React.FC<InputProps> = ({ placeholder, value, onChange, ...rest }) => {
     return (
