8917264aa2d21aed1d2678ae95b70324e65bcbd0
Quadr1on <adornsgeorge@gmail.com>
2025-02-01 23:05:14 +0530

    version 0.1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import LandingPage from './components/Landing'
 import AuthComponent from './components/Login'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-    <LandingPage   />
-
-    </>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path='/login' element={<AuthComponent/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
