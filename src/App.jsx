3665ec94a2abefde6cffe0e141cadcc701ab9bd2
Sidhanth Bibi <sidhanthbibi@gmail.com>
2025-02-11 23:59:29 +0530

    ver 0.2.7D - Bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,16 @@ import About from './pages/About'
 import ClubDetail from './pages/ClubDetail'
 import CreateEvent from './pages/CreateNewEvent'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
   return (
+    
     <BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
