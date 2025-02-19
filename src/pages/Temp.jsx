b5d6653dac0834010a269b9c09111041dd387c04
LennyDany-03 <lennydany3@gmail.com>
2025-02-19 18:24:07 +0530

    Test

--- src/pages/Temp.jsx
+++ src/pages/Temp.jsx
@@ -1,46 +1,11 @@
-// Example usage in a React component
-import { useState, useEffect } from 'react'
-import { supabase } from '../supabaseClient'
+import React from 'react'
 
-function ExampleComponent() {
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  async function fetchData() {
-    try {
-      const { data, error } = await supabase
-        .from('Events')
-        .select('*')
-
-      if (error) throw error
-
-      setData(data)
-    } catch (error) {
-      setError(error.message)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error: {error}</div>
-
+const Temp = () => {
   return (
     <div>
-      {/* Render your data here */}
-      {data.map((club,index) => (
-        <div className='flex gap-10' key={index}>
-          <h2 className='text-white'>{club.event_name}</h2>
-          <p className='text-white'>{club.description}</p>
-        </div>
-      ))}
+      
     </div>
   )
 }
 
-export default ExampleComponent
+export default Temp
